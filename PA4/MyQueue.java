fcbd7a7fea2bd4d31dd499cc755face2eb18d5df
mwdai049 <129794811+mwdai049@users.noreply.github.com>
2024-04-22 20:37:13 -0700

    add PA4 starter

---
+++ PA4/MyQueue.java
@@ -0,0 +1,52 @@
+/*
+    Name: TODO
+    PID:  TODO
+ */
+
+/**
+ * TODO
+ *
+ * @author TODO
+ * @since TODO
+ */
+
+public class MyQueue<T> {
+    
+    /* instance variables, feel free to add if you need */
+
+    /* ===separation=== */
+
+    public MyQueue() {
+        // TODO
+    }
+
+    public boolean isEmpty() {
+        // TODO
+        return false;
+    }
+
+    public int size() {
+        // TODO
+        return 0;
+    }
+
+    public void enqueue(T data) {
+        // TODO
+    }
+
+    public T dequeue() {
+        // TODO
+        return null;
+    }
+
+    public T peek() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        // TODO
+        return null;
+    }
+}
